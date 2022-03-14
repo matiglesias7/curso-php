dc49bf8775ed1444c07a9e04f6e3daebd3609562
Matias Iglesias <mat.iglesias@duocuc.cl>
2022-03-13 23:02:37 -0300

    proyecto

--- proyecto/sql/crea_tablas.sql
+++ proyecto/sql/crea_tablas.sql
@@ -1,33 +1,33 @@
 CREATE TABLE USUARIOS (
-    id              int (255) auto_increment not null,
-    nombre          varchar(100) not null,
-    apellidos       varchar(100) not null,
-    email           varchar(255) not null,
-    contraseña      varchar(255) not null,
-    fecha_registro  date not null
+    id              int (10) auto_increment not null,
+    nombre          varchar(25) not null,
+    apellidos       varchar(25) not null,
+    email           varchar(35) not null,
+    contraseña      varchar(10) not null,
+    fecha_registro  date not null,
     
-    CONSTRAINT pk_usuarios PRIMARY KEY(id) /* INDICAR LA PK DE LA TABLA */
+    CONSTRAINT pk_usuarios PRIMARY KEY(id), /* INDICAR LA PK DE LA TABLA */
     
-    CONSTRAINT uq_email UNIQUE (email); /*No ingresar un email ya existente*/
+    CONSTRAINT uq_email UNIQUE (email) /*No ingresar un email ya existente*/
 )
 
 CREATE TABLE CATEGORIAS(
-    id         int(255) auto_increment not null,
-    nombre     varchar(100) not null
+    id         int(10) auto_increment not null,
+    nombre     varchar(50) not null,
     
     CONSTRAINT pk_categorias PRIMARY KEY(id)
 )
 
 CREATE TABLE ENTRADAS(
-    id              int(255) auto_increment null,
-    usuario_id      int(255),
-    categoria_id    int(255),
-    titulo          varchar(255) not null,
+    id              int(10) auto_increment not null,
+    usuario_id      int(10),
+    categoria_id    int(10),
+    titulo          varchar(50) not null,
     descripcion     MEDIUMTEXT,
-    fecha           date not null
+    fecha           date not null,
 
-    CONSTRAINT pk_entradas PRIMARY KEY(id);
-    CONSTRAINT fk_entrada_usuario FOREIGN KEY(usuario_id) REFERENCES usuarios(id) /* INDICAR LA FK DE LAS OTRAS TABLAS); */
+    CONSTRAINT pk_entradas PRIMARY KEY(id),
+    CONSTRAINT fk_entrada_usuario FOREIGN KEY(usuario_id) REFERENCES usuarios(id), /* INDICAR LA FK DE LAS OTRAS TABLAS); */
     CONSTRAINT fk_entrada_categoria FOREIGN KEY(categoria_id) REFERENCES categorias(id)
 
 )
